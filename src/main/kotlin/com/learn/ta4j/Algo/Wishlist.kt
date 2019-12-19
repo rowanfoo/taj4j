bc5bd3af4e9037e4125a1dd60b95623572295510
rowanfoo <rowanfoo@gmail.com>
2019-12-19 21:05:22 +0800

    add

---
+++ src/main/kotlin/com/learn/ta4j/Algo/Wishlist.kt
@@ -0,0 +1,17 @@
+package com.learn.ta4j.Algo
+
+
+//public class Wishlist (var codes:List<String>) {
+//
+//
+//
+//    fun JsonObject process():JsonNode{
+//        val mapper = ObjectMapper()
+//        var node = mapper.createObjectNode()
+//
+//    }
+//
+//
+//
+//
+//}
---
+++ src/main/kotlin/com/learn/ta4j/Controller/FundamentalController.kt
@@ -0,0 +1,28 @@
+package com.learn.ta4j.Controller
+
+import com.dhamma.pesistence.entity.data.Fundamental
+import com.dhamma.pesistence.entity.data.QFundamental
+import com.dhamma.pesistence.entity.repo.FundamentalRepo
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+
+
+@CrossOrigin
+@RestController
+class FundamentalController {
+
+    @Autowired
+    lateinit var fundamentalRepo: FundamentalRepo
+
+    @GetMapping("/fundamental/{code}")
+    fun getCode(@PathVariable code: String): Fundamental = fundamentalRepo.findOne(QFundamental.fundamental.code.eq(code)).get()
+
+    @PutMapping("/fundamental")
+    fun insert(@RequestBody fundamental: Fundamental) {
+        println("----insert-------------$fundamental")
+        fundamentalRepo.save(fundamental)
+
+    }
+
+
+}

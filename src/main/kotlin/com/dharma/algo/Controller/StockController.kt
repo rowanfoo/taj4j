baae34da5196c66b2f732411ea97cce36d338273
rowanfoo <rowanfoo@gmail.com>
2019-12-31 08:45:42 +0800

    add Stocks

---
+++ src/main/kotlin/com/dharma/algo/Controller/StockController.kt
@@ -0,0 +1,29 @@
+package com.dharma.algo.Controller
+
+import com.dhamma.pesistence.entity.data.CoreStock
+import com.dharma.algo.data.MetaData
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@CrossOrigin
+@RestController
+class StockController {
+    @Autowired
+    lateinit var metadata: MetaData
+
+    @Autowired
+    lateinit var allStocks: Map<String, CoreStock>
+
+
+    @GetMapping("/stocks")
+    fun getStocks(): Set<String> {
+        var list = mutableListOf<String>()
+        return allStocks.keys
+
+    }
+
+
+}

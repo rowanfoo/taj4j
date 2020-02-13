0270511d02b82f082258dcc2afdc5884771e68a9
rowanfoo <rowanfoo@gmail.com>
2020-02-13 21:56:30 +0800

    add multiple find codes

--- src/main/kotlin/com/dharma/algo/service/AlgoService.kt
+++ src/main/kotlin/com/dharma/algo/service/AlgoService.kt
@@ -2,15 +2,11 @@ package com.dharma.algo.service
 
 import arrow.syntax.function.pipe
 import com.dhamma.ignitedata.service.NewsIgniteService
-import com.dhamma.pesistence.entity.repo.FundamentalRepo
 import com.dhamma.pesistence.service.FundamentalService
 import com.dharma.algo.data.pojo.techstr
 import com.dharma.algo.utility.GJson
-import com.dharma.algo.utility.Json
 import com.dharma.algo.utility.StringUtility.threeElems
 import com.dharma.algo.utility.StringUtility.twoElems
-import com.fasterxml.jackson.databind.node.ObjectNode
-import com.google.gson.JsonObject
 import org.apache.ignite.Ignite
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -59,18 +55,18 @@ class AlgoService {
 //        content.addProperty("volumema", arg2)
 //        content.addProperty("volumex", arg1)
 
-    var content = GJson.toGson(mapOf("volumema" to arg2 , "volumex" to arg1 ))
+        var content = GJson.toGson(mapOf("volumema" to arg2, "volumex" to arg1))
 
 
-    println("---------------MA----vola-$arg2------volx--$arg1-")
+        println("---------------MA----vola-$arg2------volx--$arg1-")
 
         lateinit var sector: String;
         if (!sectorparam.isPresent) content.addProperty("sector", "300")
         else content.addProperty("sector", sectorparam.get())
 
 //        var tech = volume.process(content)
-   // return addNews(volume.process(content))
-    return volume.process(content) pipe ::addNews  pipe  ::addFundamental
+        // return addNews(volume.process(content))
+        return volume.process(content) pipe ::addNews pipe ::addFundamental
 //        return tech
     }
 
@@ -79,10 +75,10 @@ class AlgoService {
 //        content.addProperty("time", "14")
 //        content.addProperty("percent", 0.05)
 //        content.addProperty("sector", 300)
-        var content = GJson.toGson(mapOf("sector" to 300 , "percent" to 0.05 , "time" to "14"))
+        var content = GJson.toGson(mapOf("sector" to 300, "percent" to 0.05, "time" to "14"))
         // return fallPeriod.process(content)
-      //  return addNews(fallPeriod.process(content))
-        return fallPeriod.process(content) pipe ::addNews  pipe  ::addFundamental
+        //  return addNews(fallPeriod.process(content))
+        return fallPeriod.process(content) pipe ::addNews pipe ::addFundamental
     }
 
     fun falllong(): List<techstr> {
@@ -90,10 +86,10 @@ class AlgoService {
 //        content.addProperty("time", "360")
 //        content.addProperty("percent", 0.3)
 //        content.addProperty("sector", 300)
-        var content = GJson.toGson(mapOf("sector" to 300 , "percent" to 0.35 , "time" to "360"))
-       // return fallPeriod.process(content)
+        var content = GJson.toGson(mapOf("sector" to 300, "percent" to 0.35, "time" to "360"))
+        // return fallPeriod.process(content)
         //return addNews(fallPeriod.process(content))
-        return fallPeriod.process(content) pipe ::addNews  pipe  ::addFundamental
+        return fallPeriod.process(content) pipe ::addNews pipe ::addFundamental
     }
 
 
@@ -105,15 +101,15 @@ class AlgoService {
 //        var a = JsonObject()
 //        a.addProperty("rsi", arg2)
 //        a.addProperty("rsialgo", arg1)
-        var a = GJson.toGson(mapOf("rsi" to arg2 , "rsialgo" to arg1 ))
+        var a = GJson.toGson(mapOf("rsi" to arg2, "rsialgo" to arg1))
         lateinit var sector: String;
         if (!sectorparam.isPresent) a.addProperty("sector", "300")
         else a.addProperty("sector", sectorparam.get())
 
 //        var tech = rsialgo.process(a)
 //        return tech
-       // return addNews(rsialgo.process(a))
-        return rsialgo.process(a) pipe ::addNews  pipe  ::addFundamental
+        // return addNews(rsialgo.process(a))
+        return rsialgo.process(a) pipe ::addNews pipe ::addFundamental
     }
 
     fun price(algo: String, sectorparam: Optional<String>): List<techstr> {
@@ -121,31 +117,31 @@ class AlgoService {
         var (arg1, arg2) = twoElems(algo)
 //        var content = JsonObject()
 //        content.addProperty("price", arg1)
-        var content = GJson.toGson(mapOf("price" to arg1 ))
+        var content = GJson.toGson(mapOf("price" to arg1))
 
         lateinit var sector: String;
         if (!sectorparam.isPresent) content.addProperty("sector", "300")
         else content.addProperty("sector", sectorparam.get())
 
-      //  return pricefall.process(content)
-      //  return addNews(pricefall.process(content))  pipe  addFundamental(data:List<techstr>)
-        return pricefall.process(content) pipe ::addNews  pipe  ::addFundamental
+        //  return pricefall.process(content)
+        //  return addNews(pricefall.process(content))  pipe  addFundamental(data:List<techstr>)
+        return pricefall.process(content) pipe ::addNews pipe ::addFundamental
 
     }
-    private fun addNews(data:List<techstr>):List<techstr>{
+
+    private fun addNews(data: List<techstr>): List<techstr> {
         return data.map {
             it.news = newsIgniteService.newsToday(it.code)
             it
         }.toList()
     }
-    private fun addFundamental(data:List<techstr>):List<techstr>{
+
+    private fun addFundamental(data: List<techstr>): List<techstr> {
         return data.map {
             it.fundamental = fundamentalService.code(it.code)
             it
         }.toList()
     }
-
-
 
 
     fun data() {
@@ -156,7 +152,6 @@ class AlgoService {
         var cache3 = ignite.getOrCreateCache<String, Double>("MA60:volume")
         println("--------TOTAL CACHE--------${cache0.size()}------${cache1.size()}----${cache2.size()}----${cache3.size()}----")
     }
-
 
 
 //    private fun getThreeElems(zz: String): Triple<String, String, String> {

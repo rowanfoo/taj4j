aec031cd316751a41b6da8c30b775de60547bdef
rowanfoo <rowanfoo@gmail.com>
2020-09-27 21:43:37 +0800

    clean up

--- src/main/kotlin/com/dharma/algo/service/WishlistService.kt
+++ src/main/kotlin/com/dharma/algo/service/WishlistService.kt
@@ -9,19 +9,18 @@ import com.dhamma.pesistence.entity.repo.UserRepo
 import com.dhamma.pesistence.entity.repo.WishlistRepo
 import com.dharma.algo.data.pojo.techstr
 import com.dharma.algo.utility.Json
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.PathVariable
 import java.util.*
 
 
-@Controller
+@Component
 class WishlistService {
     @Autowired
     lateinit var userRepo: UserRepo
@@ -38,38 +37,6 @@ class WishlistService {
 
     @Autowired
     lateinit var coreDataIgniteService: CoreDataIgniteService
-    var mapper = ObjectMapper()
-
-    private fun indicatorMap(username: String): Triple<Map<String, techstr>, Map<String, techstr>, Map<String, techstr>> {
-        var userconfig = user(username).userConfig
-        var rsistring = userconfig.get("rsi")?.get(0)?.asJsonObject?.get("value")?.asString
-        var falldailystring = userconfig.get("falldaily")?.get(0)?.asJsonObject?.get("value")?.asString
-        var volumexstring = userconfig.get("volumex")?.get(0)?.asJsonObject?.get("value")?.asString
-        lateinit var rsilist: Map<String, techstr>
-        lateinit var falldailylist: Map<String, techstr>
-        lateinit var volumexlist: Map<String, techstr>
-
-        runBlocking {
-
-            launch {
-                if (rsistring != null) rsilist = algoService.rsi(rsistring, Optional.empty()).map { it.code to it }.toMap()
-            }
-
-            launch {
-                println("---------falldailystring---------------$falldailystring-----")
-                if (falldailystring != null) falldailylist = algoService.price(falldailystring, Optional.empty()).map { it.code to it }.toMap()
-                println("---------falldailystring------size---------${falldailylist.size}-----")
-            }
-
-            launch {
-                println("---------Y1-----------------")
-                if (volumexstring != null) volumexlist = algoService.vol(volumexstring, Optional.empty()).map { it.code to it }.toMap()
-                println("---------Y2-----------------")
-
-            }
-        }
-        return Triple(rsilist, falldailylist, volumexlist)
-    }
 
 
     fun wishlistsummary(username: String): ArrayNode {
@@ -85,8 +52,8 @@ class WishlistService {
 //        var falldailystring = userconfig.get("falldaily")?.get(0)?.asJsonObject?.get("value")?.asString
 //        var volumexstring = userconfig.get("volumex")?.get(0)?.asJsonObject?.get("value")?.asString
 
-        var list = mutableListOf<JsonNode>()
-        var map = mutableMapOf<String, String>()
+        // var list = mutableListOf<JsonNode>()
+        //var map = mutableMapOf<String, String>()
 
 //        lateinit var rsilist: Map<String, techstr>
 //        lateinit var falldailylist: Map<String, techstr>
@@ -145,6 +112,36 @@ class WishlistService {
         return arrayNode
     }
 
+    private fun indicatorMap(username: String): Triple<Map<String, techstr>, Map<String, techstr>, Map<String, techstr>> {
+        var userconfig = user(username).userConfig
+        var rsistring = userconfig.get("rsi")?.get(0)?.asJsonObject?.get("value")?.asString
+        var falldailystring = userconfig.get("falldaily")?.get(0)?.asJsonObject?.get("value")?.asString
+        var volumexstring = userconfig.get("volumex")?.get(0)?.asJsonObject?.get("value")?.asString
+        lateinit var rsilist: Map<String, techstr>
+        lateinit var falldailylist: Map<String, techstr>
+        lateinit var volumexlist: Map<String, techstr>
+
+        runBlocking {
+
+            launch {
+                if (rsistring != null) rsilist = algoService.rsi(rsistring, Optional.empty()).map { it.code to it }.toMap()
+            }
+
+            launch {
+                println("---------falldailystring---------------$falldailystring-----")
+                if (falldailystring != null) falldailylist = algoService.price(falldailystring, Optional.empty()).map { it.code to it }.toMap()
+                println("---------falldailystring------size---------${falldailylist.size}-----")
+            }
+
+            launch {
+                println("---------Y1-----------------")
+                if (volumexstring != null) volumexlist = algoService.vol(volumexstring, Optional.empty()).map { it.code to it }.toMap()
+                println("---------Y2-----------------")
+
+            }
+        }
+        return Triple(rsilist, falldailylist, volumexlist)
+    }
 
     private fun message(rsilist: Map<String, techstr>, falldailylist: Map<String, techstr>, volumexlist: Map<String, techstr>, code: String): String {
         var message = rsilist.get(code)?.message ?: ""
@@ -165,9 +162,9 @@ class WishlistService {
 //    }
 
 
-    private fun getbydate(@PathVariable algo: String, sectorparam: Optional<String>): List<techstr> {
-        return algoService.rsi(algo, sectorparam)
-    }
+//    private fun getbydate(@PathVariable algo: String, sectorparam: Optional<String>): List<techstr> {
+//        return algoService.rsi(algo, sectorparam)
+//    }
 
 
     private fun allfavs(username: String): List<String> {
@@ -182,9 +179,9 @@ class WishlistService {
 
     private fun user(username: String): User = userRepo.findOne(QUser.user.username.eq(username)).get()
 
-    private fun ris(algo: String) {
-        algoService.rsi(algo, Optional.empty())
-    }
+//    private fun ris(algo: String) {
+//        algoService.rsi(algo, Optional.empty())
+//    }
 
 
 }

126a882ce31e4c8b69854fd3f641e666a6a9af52
rowanfoo <rowanfoo@gmail.com>
2020-10-09 16:56:10 +0800

    migrating all to use historical_indicators

--- src/main/kotlin/com/dharma/algo/Controller/WishlistController.kt
+++ src/main/kotlin/com/dharma/algo/Controller/WishlistController.kt
@@ -1,16 +1,17 @@
 package com.dharma.algo.Controller
 
+import com.dhamma.pesistence.entity.data.QWishlist
+import com.dhamma.pesistence.entity.data.Wishlist
+import com.dhamma.pesistence.entity.repo.WishlistRepo
 import com.dhamma.pesistence.service.FundamentalService
-import com.dharma.algo.service.algo.MetaData
 import com.dharma.algo.service.WishlistService
+import com.dharma.algo.service.algo.MetaData
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.ArrayNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.util.*
 
 
 @CrossOrigin
@@ -24,6 +25,10 @@ class WishlistController {
 
     @Autowired
     lateinit var wishlistService: WishlistService
+
+    @Autowired
+    lateinit var wishlistRepo: WishlistRepo
+
 
     @GetMapping("/wishlist/metadata/{stocks}")
 //    fun getMetaData(@PathVariable stocks: String): ArrayNode {
@@ -50,16 +55,27 @@ class WishlistController {
             (towork as ObjectNode).put("yearchange", it.yearchange ?: 0.00)
             map[it.code] = towork
         }
-
-
-
         println("---------------------RUN----------JSON--------$z----")
 //        return z
         return map.values.asIterable()
     }
+//    ---------getWishlisSummary-------http://localhost:8080/wishlist/alldetails/rowan
+
+//    @GetMapping("/wishlist/alldetails/{userid}")
+//    fun getalldetails(@PathVariable userid: String): ArrayNode {
+//        return wishlistService.wishlistsummary(userid)
+//    }
 
     @GetMapping("/wishlist/alldetails/{userid}")
-    fun getalldetails(@PathVariable userid: String): ArrayNode {
-        return wishlistService.wishlistsummary(userid)
+    fun getalldetails(@PathVariable userid: String, @RequestParam date: Optional<String>): ArrayNode {
+        return wishlistService.wishlistsummary(userid, date)
     }
+
+    @GetMapping("/wishlist/wishlistcategorys/{userid}")
+    fun wishlistcategorys(@PathVariable userid: String): List<Wishlist> {
+        return wishlistRepo.findAll(QWishlist.wishlist.userid.eq(userid)).toList()
+    }
+
+
 }
+

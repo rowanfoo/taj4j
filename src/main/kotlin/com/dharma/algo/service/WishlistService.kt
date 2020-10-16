82f4f5df5e6d15dac17e9433d7ac273c3c94e863
rowanfoo <rowanfoo@gmail.com>
2020-10-16 11:22:53 +0800

    add date

--- src/main/kotlin/com/dharma/algo/service/WishlistService.kt
+++ src/main/kotlin/com/dharma/algo/service/WishlistService.kt
@@ -35,7 +35,6 @@ class WishlistService {
     lateinit var coreDataIgniteService: CoreDataIgniteService
 
     @Autowired
-
     lateinit var historyIndicatorService: HistoryIndicatorService
 
     fun wishlistsummary(username: String, date: Optional<String>): ArrayNode {
@@ -89,70 +88,24 @@ class WishlistService {
             //            var message = rsilist.get(it)?.message ?: ""
 //            message = message + " " + (falldailylist.get(it)?.message ?: "")
 //            message = message + " " + (volumexlist.get(it)?.message ?: "")
-            println("---------XXX---code-----------$it---")
 
             var rootNode = mapper.createObjectNode()
             (rootNode as ObjectNode).put("code", it)
 //            (rootNode as ObjectNode).put("message", message)
             (rootNode as ObjectNode).put("message", message(map, it))
-
-            println("---------ZzzZZZee---1--------------")
-            var data = coreDataIgniteService.today(it)
-            println("---------ZzzZZZee---1--------$data------")
+            var data = coreDataIgniteService.dateeq(it, mydate.toString())
             (rootNode as ObjectNode).put("price", data.close)
 
             (rootNode as ObjectNode).put("change", data.changepercent)
 
             (rootNode as ObjectNode).put("volume", data.volume)
             (rootNode as ObjectNode).put("date", data.date.toString())
-            println("---------ZzzZZZee---2--------------")
-//            var a = JsonObject()
-//            a.addProperty("code", it)
-//            a.addProperty("date", data.date.toString())
-//            (rootNode as ObjectNode).put("news", mapper.convertValue(newsIgniteService.getCode(a), JsonNode::class.java))
             (rootNode as ObjectNode).put("news", Json.toJson(newsIgniteService.newsToday(it)))
-
-
             arrayNode.add(rootNode)
         }
 
         return arrayNode
     }
-
-//    private fun indicatorMap(username: String): Triple<Map<String, techstr>, Map<String, techstr>, Map<String, techstr>> {
-////        var userconfig = user(username).userConfig
-////        var rsistring = userconfig.get("rsi")?.get(0)?.asJsonObject?.get("value")?.asString
-////        var falldailystring = userconfig.get("falldaily")?.get(0)?.asJsonObject?.get("value")?.asString
-////        var volumexstring = userconfig.get("volumex")?.get(0)?.asJsonObject?.get("value")?.asString
-//        lateinit var rsilist: Map<String, techstr>
-//        lateinit var falldailylist: Map<String, techstr>
-//        lateinit var volumexlist: Map<String, techstr>
-////
-////        runBlocking {
-////
-////            launch {
-////                if (rsistring != null) rsilist = algoService.rsi(rsistring, Optional.empty()).map { it.code to it }.toMap()
-////            }
-////
-////            launch {
-////                println("---------falldailystring---------------$falldailystring-----")
-////                if (falldailystring != null) falldailylist = algoService.price(falldailystring, Optional.empty()).map { it.code to it }.toMap()
-////                println("---------falldailystring------size---------${falldailylist.size}-----")
-////            }
-////
-////            launch {
-////                println("---------Y1-----------------")
-////                if (volumexstring != null) volumexlist = algoService.vol(volumexstring, Optional.empty()).map { it.code to it }.toMap()
-////                println("---------Y2-----------------")
-////
-////            }
-////        }
-////        return Triple(rsilist, falldailylist, volumexlist)
-//
-//
-//        return Triple(rsilist, falldailylist, volumexlist)
-//
-//    }
 
 
     private fun message(map: Map<String, List<HistoryIndicators>>, code: String): String {
@@ -175,31 +128,6 @@ class WishlistService {
 
     }
 
-
-//    private fun message(rsilist: Map<String, techstr>, falldailylist: Map<String, techstr>, volumexlist: Map<String, techstr>, code: String): String {
-//        var message = rsilist.get(code)?.message ?: ""
-//        message = message + " " + (falldailylist.get(code)?.message ?: "")
-//        message = message + " " + (volumexlist.get(code)?.message ?: "")
-//        return message
-//    }
-
-
-//    private fun newsToday(code: String): JsonNode {
-//        var mapper = ObjectMapper()
-//        var data = coreDataIgniteService.today(code)
-//
-//        var a = JsonObject()
-//        a.addProperty("code", code)
-//        a.addProperty("date", data.date.toString())
-//        return mapper.convertValue(newsIgniteService.getCode(a), JsonNode::class.java)
-//    }
-
-
-//    private fun getbydate(@PathVariable algo: String, sectorparam: Optional<String>): List<techstr> {
-//        return algoService.rsi(algo, sectorparam)
-//    }
-
-
     private fun allfavs(username: String): List<String> {
         var mutableList = mutableListOf<String>()
 
@@ -211,10 +139,6 @@ class WishlistService {
     }
 
     private fun user(username: String): User = userRepo.findOne(QUser.user.username.eq(username)).get()
-
-//    private fun ris(algo: String) {
-//        algoService.rsi(algo, Optional.empty())
-//    }
 
 
 }

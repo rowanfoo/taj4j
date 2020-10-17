564f8c5fffef60cf4fb207e0febb63933ea4fc50
rowanfoo <rowanfoo@gmail.com>
2020-10-17 12:54:42 +0800

    add merge function

--- src/main/kotlin/com/dharma/algo/utility/GJson.kt
+++ src/main/kotlin/com/dharma/algo/utility/GJson.kt
@@ -1,6 +1,7 @@
 package com.dharma.algo.utility
 
 import com.google.gson.Gson
+import com.google.gson.JsonObject
 
 
 object GJson {
@@ -14,5 +15,23 @@ object GJson {
 
     fun toGson(obj: Any) = gson.toJsonTree(obj).asJsonObject
 
+
+    fun merge(a: JsonObject, b: JsonObject, exclude: List<String>): JsonObject {
+//       var map = a. toMap();
+//        map.putAll(b.toMap());
+//        JSONObject combined = new JSONObject(map);
+
+
+//        val firstObject: HashMap<*, *> = Gson().fromJson(a, HashMap::class.java)
+//        val secondObject: Map<Nothing, Nothing> = Gson().fromJson(b, HashMap::class.java)
+//        firstObject.putAll(secondObject)
+        a.entrySet().forEach {
+            if (!exclude.contains(it.key)) {
+                b.addProperty(it.key, it.value.asString)
+            }
+
+        }
+        return a
+    }
 
 }

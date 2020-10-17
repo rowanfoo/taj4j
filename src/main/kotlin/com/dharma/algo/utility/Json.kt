df3036321ee0338a5558d78969db5902a67cdb5e
rowanfoo <rowanfoo@gmail.com>
2020-10-17 12:54:18 +0800

    Add merge func

--- src/main/kotlin/com/dharma/algo/utility/Json.kt
+++ src/main/kotlin/com/dharma/algo/utility/Json.kt
@@ -2,17 +2,21 @@ package com.dharma.algo.utility
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
 
- object Json{
-     var mapper = ObjectMapper()
+object Json {
+    var mapper = ObjectMapper()
 
-     fun toJson(obj:Any):JsonNode{
-         return mapper.convertValue(obj, JsonNode::class.java)
-     }
+    fun toJson(obj: Any): JsonNode {
+        return mapper.convertValue(obj, JsonNode::class.java)
+    }
 
-//     fun toJson(obj:Map<Any,Any>):JsonNode{
+    //     fun toJson(obj:Map<Any,Any>):JsonNode{
 //         return mapper.valueToTree(map)
 //     }
 //
-
+    fun merge(a: ObjectNode, b: ObjectNode, exclude: List<String>): ObjectNode {
+        b.remove(exclude)
+        return a.setAll(b) as ObjectNode
+    }
 }

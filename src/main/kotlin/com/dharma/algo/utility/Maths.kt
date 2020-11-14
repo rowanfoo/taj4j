6dee441172408a846f50381fc22451660884f2d1
rowanfoo <rowanfoo@gmail.com>
2020-11-14 12:57:23 +0800

    add percent format

--- src/main/kotlin/com/dharma/algo/utility/Maths.kt
+++ src/main/kotlin/com/dharma/algo/utility/Maths.kt
@@ -2,4 +2,6 @@ package com.dharma.algo.utility
 
 object Maths {
     fun percent(currentprrice: Double, vsprrice: Double) = String.format("%.3f", (currentprrice - vsprrice) / vsprrice).toDouble()
+
+    fun percentformat(d: Double) = String.format("%.3f", d)
 }
